cdf0b9f62f166d1fbc18a30252e52ccc91b36678
Maxime Rose <maxime.rose@hotmail.fr>
2025-11-01 22:27:00 +0100

    Make all links works, and little improvements

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const LINKS = {
+  github: "https://github.com/maaxow",
+  linkedin: "https://www.linkedin.com/in/maxime-rose/",
+  benhance_elodie: "https://www.behance.net/elodietraulet1"
+}
+
+function openLinkButton(link) {
+  window.open(link, "_blank");
+}
